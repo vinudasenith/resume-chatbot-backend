c698f05c2ed0e7b3c694c141708c2895d58839e4
vinudasenith <ha.vinudas@gmail.com>
2025-09-15 20:51:25 +0530

    add cors✌️

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Local AI Chatbot",
@@ -7,3 +8,13 @@ app = FastAPI(
 )
 
 app.include_router(chat.router, prefix="/api/v1")
+
+#add cors configurations
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
